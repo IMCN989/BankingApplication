178427597570fef92fb951fb6c64c03401d2d749
maccer989 <132687553+maccer989@users.noreply.github.com>
2025-05-25 17:35:50 +0100

    Add project files.

---
+++ BankingApp.ATM.Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BankingApp.ATM.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ BankingApp.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BankingApp.Core
+{
+    public class Class1
+    {
+
+    }
+}
